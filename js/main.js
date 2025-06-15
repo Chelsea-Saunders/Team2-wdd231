7ed239cc68e7b3903b54acb494ae43d2c28b8d9f
Chelsea-Saunders <168315598+Chelsea-Saunders@users.noreply.github.com>
2025-06-14 19:46:35 -0600

    added setupSkillDropdown to DOM in main.js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,15 @@
-// MAIN.JS
 import { setupHamburgerMenu, setupResumeModals, setupSkillDropdown } from "./index.js";
 import { initModal, createNewAccount } from "./login-modal.js"
 
+
 console.log("main.js is running");
 
-setupHamburgerMenu();
-setupResumeModals();
-initModal();
-createNewAccount();
-setupSkillDropdown();
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("main.js is running");
+
+    setupHamburgerMenu();
+    setupResumeModals();
+    initModal();
+    createNewAccount();
+    setupSkillDropdown();
+});
